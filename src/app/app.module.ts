8cd87d4ef3f23efbc8b059a8e5006fa93b04e2a5
Andre Sales <andrelsales@gmail.com>
2019-11-29 17:13:43 -0300

    commit parcial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import {HttpClientModule} from '@angular/common/http';
 import { MyApp } from './app.component';
 import { ListPage } from '../pages/list/list';
 
@@ -15,6 +15,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp),
   ],
   bootstrap: [IonicApp],
