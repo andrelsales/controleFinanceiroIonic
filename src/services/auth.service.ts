a8159c84732503645d2721beb1c8529e402a9e70
Andre Sales <andrelsales@gmail.com>
2019-12-05 16:28:55 -0300

    Fazendo o app aproveitar o usuário

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -26,6 +26,16 @@ export class AuthService {
                 responseType: 'text'
             });
     }   
+
+    refreshToken() {
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/auth/refresh_token`, 
+            {},
+            {
+                observe: 'response',
+                responseType: 'text'
+            });
+    }   
     successfulLogin(authorizationValue : string) {
         console.log("successfulLogin");
         let tok = authorizationValue.substring(7);
